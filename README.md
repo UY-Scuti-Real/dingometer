40d31f00080ada3b2e67b95d72574d2127aa85ab
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-10-24 19:20:56 +0100

    + imitial structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dingometer
+
 code review helper
+
+## TL;DR
+
+When taking over a project, it is useful to mark things for review.
+dingometer automates this by checking the code for `DING` occurances (written
+by you), and outputting the file path, line number and line to a file.
---
+++ __main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Author: Richard A 
+Date: 2019-10-07
+
+This package goes through the source directory file by file and finds all
+the "DING"s,  and produces a file containing:
+
+1) The relative path of the file it is in
+2) The line it is on
+3) the contents of that line
+"""
