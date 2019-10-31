7787befb2b53ce224295b4e0b299cca22deca2e8
UY Scuti <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-10-31 19:04:16 +0000

    + prints dings/scores to console

--- __main__.py
+++ __main__.py
@@ -3,10 +3,79 @@
 Author: Richard A 
 Date: 2019-10-07
 
+Usage:
+Run this module, first positional argument is the path to be searched. If no
+argument is present, the script will assume the current directory is the
+root directory of the package.
+
 This package goes through the source directory file by file and finds all
 the "DING"s,  and produces a file containing:
 
 1) The relative path of the file it is in
 2) The line it is on
 3) the contents of that line
+
+
 """
+import os
+import sys
+import term_finder
+
+
+
+# returns current working directory as str
+def _get_current_working_directory():
+    """
+    returns the current working directory
+
+    This is a 1 line function, but it exists incase 
+    I have to mess around with it later. 
+    """
+    return os.getcwd()
+
+DIRECTORY = _get_current_working_directory()
+FILE_EXTENSIONS = [".py", ".c", ".h"]
+
+# returns the directory passed to the program
+def _get_passed_directory():
+    """
+    should only be called if len(sys.argv) > 1.  
+    """
+    return sys.argv[1]
+
+
+# sets global directory, extensions from sys args
+def _set_dir_exts_from_args():
+    """
+    updates the global variables. Of course, globals are undesirable but given
+    these variables are used everywhere, they should remain in the global name
+    space.
+    """
+    global DIRECTORY, FILE_EXTENSIONS
+    # Richard's patented method for python case statements
+    # search_directory = argv_len_switcher[argv_len_case]()
+    argv_len = len(sys.argv)
+    if (argv_len > 1):
+        DIRECTORY = _get_passed_directory()
+    if (argv_len > 2):
+        _extensions = []
+        for file_extension_arg in sys.argv[2:]:
+            _extensions.append(file_extension_arg)
+        FILE_EXTENSIONS = _extensions
+
+
+def main():
+    """
+    Main function of the package is used to do the thing
+    """
+    term = "DING"
+    # print(DIRECTORY)
+    paths = term_finder.get_file_paths_to_check(DIRECTORY, FILE_EXTENSIONS, [])
+    # print(paths)
+    file_details_list = term_finder.assess_paths(term, paths)
+    print(file_details_list)
+
+
+if __name__ == "__main__":
+    _set_dir_exts_from_args()
+    main()
--- term_finder.py
+++ term_finder.py
@@ -7,42 +7,82 @@ import os
 from strings import filter_strings
 
 
-# gobals ---------------------------------------------------------------------
-
-
-term_to_find = None
-inclusions = []
-exclusions = []
-# test value please ignore
-_root_directory = __file__.split("dingometer\\")[-1]
-
-
 # classes --------------------------------------------------------------------
-
-class _line:
-    def __init__(self, path_str, line_number, line_string):
+class file_details:
+    def __init__(self, file_path: str, lines_in_file: int, term_objs_in_file: list):
         self.__dict__.update(locals())
+        self.terms_in_file = len(term_objs_in_file)
+        self.score = self._get_file_score()
+
+    def _get_file_score(self):
+        return self.lines_in_file/self.terms_in_file
+
+    def __repr__(self):
+        out_string = f"File: {self.file_path}\tscore: {self.score}\n"
+        for task in self.term_objs_in_file:
+            sub_string = "--------\n"+repr(task)+"\n"
+            out_string += sub_string
+        return out_string
+
+
+
+class term_obj:
+    def __init__(self, path_str, line_number, line_string):
+        # the "local" namespace is just the variables passed to the __init__
+        # function, and the "__dict__"
+        self.__dict__.update(locals())
+
+
+    def __repr__(self):
+        return f"line contents:\n\tPath:\t{self.path_str}\
+\n\tLnNo:\t{self.line_number}\n\tLine:\t{self.line_string}"
 
 
 # functions ------------------------------------------------------------------
 
 
-def get_file_paths_to_check(root_directory):
+def get_file_paths_to_check(root_directory, inclusions, exclusions):
+    """
+    when passed a root directory get all the files of a desired type
+    """
     file_set = set()
     for current_path,\
             current_path_dirs,\
             current_path_files in os.walk(root_directory):
 
+        # all of the module creating, sub-module adding, etc was just for
+        # this one line:
         addable_files = filter_strings(
             current_path_files, inclusions, exclusions
         )
 
         for file in addable_files:
-            file_set.add(os.join(current_path, addable_files))
+            file_set.add(os.path.join(current_path, file))
+    return file_set
 
 
-def find_term_in_dir(term, root_directory):
-    pass
+def _assess_file_from_lines(term, lines_list, file_name):
+    term_obj_list = []
+    for index, line in enumerate(lines_list):
+        if term in line:
+            new_term_obj = term_obj(file_name, index+1, line)
+            term_obj_list.append(new_term_obj)
+    return term_obj_list
 
 
-print(get_file_paths_to_check(_root_directory))
+def assess_paths(term, path_list, local_returns=None):
+    # complete_terms_list = []
+    file_details_list = []
+    file_score_dict = {}
+    for path in path_list:
+        with open(path) as fp:
+            text = fp.read()
+            if "\n" in text:
+                lines = text.splitlines()
+            else:
+                continue
+        term_obj_list = _assess_file_from_lines(term, lines, path)
+        if len(term_obj_list) > 0:
+            new_file_details = file_details(path, len(lines), term_obj_list)
+            file_details_list.append(new_file_details)
+    return file_details_list
